302cea23831f390d678585de2b68c49c3bd15677
pain <pain@WIN-JQIP34S7PC5>
2017-11-29 17:32:39 +0800

    测试多个模块间的调用

---
+++ HtsMCC-core/src/main/java/com/huatech/App.java
@@ -0,0 +1,11 @@
+package com.huatech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+   public static String getClassName(){
+	   return "App.class";
+   }
+}
--- HtsMCC-web/src/main/java/com/huatech/TestController.java
+++ HtsMCC-web/src/main/java/com/huatech/TestController.java
@@ -2,4 +2,8 @@ package com.huatech;
 
 public class TestController {
 
+	public static void main(String[] args){
+		App a=new App();
+		System.out.println(a.getClassName());
+	}
 }
