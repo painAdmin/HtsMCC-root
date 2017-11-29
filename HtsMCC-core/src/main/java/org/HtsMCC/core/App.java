6bf9c19bbf1586588f6a0ec55f0f57b0c0496161
pain <pain@WIN-JQIP34S7PC5>
2017-11-29 17:10:09 +0800

    提交项目文件

---
+++ HtsMCC-core/src/main/java/org/HtsMCC/core/App.java
@@ -0,0 +1,13 @@
+package org.HtsMCC.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
